bac651a09577a9027088d546d4a2ed7c5d8d1ae6
Huy Hoang <boydaihungst@gmail.com>
2025-11-13 08:17:58 +0700

    update

--- hypr/scripts/toggle_gemini.sh
+++ hypr/scripts/toggle_gemini.sh
@@ -7,9 +7,11 @@ WINDOW_ADDRESS=$(hyprctl clients -j | jq -r '.[] | select(.class == "gemini") | 
 # Check if a window address was found
 if [ -n "$WINDOW_ADDRESS" ]; then
     # If the window exists, focus it using its address.
-    hyprctl dispatch togglespecialworkspace
+    # hyprctl dispatch togglespecialworkspace
+    hyprctl dispatch focuswindow address:"$WINDOW_ADDRESS"
 else
     # If the window does not exist, run the specified kitty command.
     # The '&' at the end runs the command in the background.
-    hyprctl dispatch exec [workspace special] "kitty --app-id 'gemini' -- gemini"
+    # hyprctl dispatch exec [workspace special] "kitty --app-id 'gemini' -- gemini"
+    hyprctl dispatch exec "kitty --app-id 'gemini' -- gemini"
 fi
---
+++ nvim/lua/plugins/ts-comments.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/ts-comments.nvim",
+  opts = {
+    lang = {
+      kitty = "# %s",
+    },
+  },
+  optional = true,
+}
