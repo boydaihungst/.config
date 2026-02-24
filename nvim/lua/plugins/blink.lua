ea70d6e6f2321bcc08b84931c0a000fc40cbf3e6
Huy Hoang <boydaihungst@gmail.com>
2026-02-25 01:48:58 +0700

    chore: fix autopairs fastwrap overlaps blink menu

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -3,6 +3,25 @@ local function has_words_before()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
+-- Check if there is any autopair fastwrap extmarks
+local function has_fastwrap_extmarks(bufnr)
+  bufnr = bufnr or 0 -- Default to current buffer
+
+  vim.api.nvim_get_namespaces()
+
+  -- Fetch extmarks in the specific namespace
+  -- Parameters: buffer, namespace_id, start_range, end_range, options
+  local extmarks = vim.api.nvim_buf_get_extmarks(
+    bufnr,
+    require("nvim-autopairs.fastwrap").ns_fast_wrap,
+    0, -- Start of buffer (row 0)
+    -1, -- End of buffer (row -1)
+    {}
+  )
+
+  return #extmarks > 0
+end
+
 ---@type LazySpec
 return {
   "saghen/blink.cmp",
@@ -222,6 +241,8 @@ return {
         end,
       },
       menu = {
+        -- NOTE: Fix autopairs fastwrap overlapping menu
+        auto_show = function(_, _) return not has_fastwrap_extmarks() end,
         draw = {
           columns = {
             { "kind_icon" },
